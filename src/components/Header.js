613114da61aba11b80bef407469d6bec79ea164f
pividmonrer <sergiodavidpj@hotmail.com>
2017-06-29 19:55:10 -0500

    Creado Header Fin de la Clase 29 Junio

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React, {Component}  from 'react';
+
+const Header = ()=> 
+(
+    <h1 className="text-center">
+      NTP - Adress Book - SERGIO PAEZ
+    </h1>
+);
+
+export default Header;
